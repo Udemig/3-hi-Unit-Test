c6dbe1b70ccbd157a212a0153774636b9b884e6c
furkanevin <furkanevin00@gmail.com>
2023-02-16 23:06:30 +0300

    style

--- src/pages/form/Form.jsx
+++ src/pages/form/Form.jsx
@@ -2,18 +2,20 @@ import React, { useState } from 'react';
 
 const Form = () => {
   const [isChecked, setIsChecked] = useState(false);
-  
+
   return (
-    <form className="mt-4 row d-flex justify-content-center align-items-center">
-      <div className="col-4 d-flex gap-4">
+    <form className=" container mt-4 mb-1 d-flex  align-items-center justify-content-between">
+      <div className=" d-flex gap-4">
         <input
           id="kosul"
           type="checkbox"
           onChange={(e) => setIsChecked(!isChecked)}
         />
-        <label htmlFor="kosul">Kosulları okudum ve kabul ediyorum</label>
+        <label htmlFor="kosul" className="text-nowrap">
+          Kosulları okudum ve kabul ediyorum
+        </label>
       </div>
-      <button disabled={!isChecked} className="col-2 btn btn-warning">
+      <button disabled={!isChecked} className="  btn btn-warning ">
         Siparişi Onayla
       </button>
     </form>
